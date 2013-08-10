7784d98cd75b40f00029d2141467436f44422ac6
Mike Murray <mmurray@madsecinc.com>
2013-08-10 13:51:46 -0700

    Error in my application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ include TheRoleController
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with :exception
+  protect_from_forgery 
 
   # your Access Denied processor
   def access_denied
