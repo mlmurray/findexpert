8d4b27e8a59607bdfccca95527470b03170508bc
Mike Murray <mmurray@madsecinc.com>
2013-08-09 23:24:10 -0700

    Initial commit Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Findexpert::Application.config.secret_key_base = 'd62106bed9a7b52689d2638ff0c3dcdd4f669d544fb04e1647d0dd8f4348bbc25c425d5767a1fe333bc0663904e6947eab5a878d3b49023ff5cc2e8e3cda12ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Findexpert::Application.config.session_store :cookie_store, key: '_findexpert_session'
