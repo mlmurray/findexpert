a0f12eeed2f89341c5151c7f72e749c67c6f3f10
Mike Murray <mmurray@madsecinc.com>
2013-08-09 23:13:39 -0700

    Re-doing this with Rails 4

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Findexpert::Application.config.secret_token = '6f09ec8835ae86b972a272ae26439ab493f1cfc266ac4dad63c779d770c31dddce0d1e9399a8e732e7d55432b874da5494e0e8e11e372387351e5387e8e17ebf'
