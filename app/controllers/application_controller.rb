6f45f0b395f4b522021706b712a3bb5cbe824a75
Mike Murray <mmurray@madsecinc.com>
2013-08-10 22:12:55 -0700

    New Config Stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery 
 
   # Redirect to the thank you for signing up page after signup.
-  #def after_sign_in_path_for(resource)
-  #  thankyou_path
-  #end
+  def after_sign_up_path_for(resource)
+    thankyou_path
+  end
 
 
 end
