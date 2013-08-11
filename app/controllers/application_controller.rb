f70e1fc2542eb4c28444a3dc6b2952aaf4579f51
Mike Murray <mmurray@madsecinc.com>
2013-08-10 21:49:13 -0700

    Change redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery 
 
+  # Redirect to the thank you for signing up page after signup.
+  def after_sign_in_path_for(resource)
+    thank_you_path
+  end
+
 
 end
