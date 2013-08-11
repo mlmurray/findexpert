0c7d21834f9c4f600807c586106a450b2fb1872d
thecrentist <thecrentist00@gmail.com>
2013-08-10 22:00:09 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Findexpert::Application.routes.draw do
 
-  devise_for :users do
-    root 'devise/registrations#new'
-  end
+  devise_for :users
 
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
