0b1774bd2bcc92a50bf9098928b592807b536f65
Mike Murray <mmurray@madsecinc.com>
2013-08-10 22:15:10 -0700

    Thank you

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery 
 
-  # Redirect to the thank you for signing up page after signup.
-  def after_sign_up_path_for(resource)
-    thankyou_path
-  end
-
 
 end
