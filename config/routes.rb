a009ca94e30fbf6fb871021c4c23a7462365f94a
Mike Murray <mmurray@madsecinc.com>
2013-08-10 22:03:52 -0700

    Changes to the signin stuff

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Findexpert::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resource :thankyou
 
   # Example resource route with options:
   #   resources :products do
