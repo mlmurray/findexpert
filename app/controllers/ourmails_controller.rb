b19053f1dbc8156fc4b459cd19b94f9a449cb1df
Mike Murray <mmurray@madsecinc.com>
2013-08-11 12:35:39 -0700

    Fixed set code

--- app/controllers/ourmails_controller.rb
+++ app/controllers/ourmails_controller.rb
@@ -1,5 +1,5 @@
 class OurmailsController < ApplicationController
-  before_action :set_mail, only: [:show, :edit, :update, :destroy]
+  before_action :set_ourmail, only: [:show, :edit, :update, :destroy]
 
   # GET /ourmails
   # GET /ourmails.json
